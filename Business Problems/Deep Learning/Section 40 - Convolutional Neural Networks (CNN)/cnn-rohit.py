0f0e8f53048bb764bb3af608d82eb497c47b1b37
rohitmaggu <maggurohit121002@gmail.com>
2019-09-17 21:28:29 +0530

    commit through bash

---
+++ Business Problems/Deep Learning/Section 40 - Convolutional Neural Networks (CNN)/cnn-rohit.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  3 13:43:52 2019
+
+@author: ROHIT
+"""
+
+#Part1- building the CNN
+from keras.models import Sequential 
+from keras.layers import Dense
+from keras.layers import Convolution2D
+
+ 
+ 
+ 
+ 
